c229e8fdfb0cf8055be388ac6d5ed8efdee353d5
Andeboizin5 <andeboizin5@gmail.com>
2023-08-04 07:39:07 -0700

    0-whatsmyname.c

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main -  prints its name
@@ -10,8 +9,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
-
