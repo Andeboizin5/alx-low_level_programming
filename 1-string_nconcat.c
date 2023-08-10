6bf803ac6f605cfb1e582a64a79f8874a7e462f0
Andeboizin5 <andeboizin5@gmail.com>
2023-08-10 07:46:06 -0700

    1-string_nconcat.c

---
+++ 1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: Always 0
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+	while (s1 && s1[k])
+		k++;
+	while (s2 && s2[l])
+		l++;
+
+	if (n < l)
+		s = malloc(sizeof(char) * (k + n + 1));
+	else
+		s = malloc(sizeof(char) * (k + l + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < k)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < l && i < (k + n))
+		s[i++] = s2[j++];
+
+	while (n >= l && i < (k + l))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+	return (s);
+}
+
