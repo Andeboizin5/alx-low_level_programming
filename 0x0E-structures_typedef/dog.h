99dca0b3279e2d9f7c7a6e520c202898cecd992f
Andeboizin5 <andeboizin5@gmail.com>
2023-08-14 09:52:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * init_dog - dog struct
+ * init_dog - struct dog with the following elements
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
@@ -10,7 +10,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(structure));
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
