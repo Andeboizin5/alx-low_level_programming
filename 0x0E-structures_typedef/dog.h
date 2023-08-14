7148601141cd64389c49cecf6809f0e89b856306
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-08-14 23:25:04 +0200

    changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,23 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - struct dog with the following elements
- * @name: dog name
- * @age: dog age
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
  * @owner: dog owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+typeof struct dog dog_t
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
+
