d53d46c8fe2178c573061f3338cd6c0cf4103163
Andeboizin5 <andeboizin5@gmail.com>
2023-08-15 02:14:09 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,9 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedof for struct dog
+ */
 typeof struct dog dog_t
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
