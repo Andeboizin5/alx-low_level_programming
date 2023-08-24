4d73183aa4ce210dfe57793e7d272be74d774ebe
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-08-24 22:00:30 +0200

    changes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * list_len -  returns the number of elements in a linked list_t list
+ * list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
@@ -18,3 +18,4 @@ size_t list_len(const list_t *h)
 	}
 	return (n);
 }
+
