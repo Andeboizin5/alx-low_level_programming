1fc2a67bf5ea94cb3343a5e8751458e85050ea97
Andeboizin5 <andeboizin5@gmail.com>
2023-09-13 08:29:10 -0700

    2-int_index.c

---
+++ 2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: size
+ * @cmp: cmp
+ * Return: Always 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
