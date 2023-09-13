38461da2dad7ea0f6981d7b756a392a39d9f93e0
Andeboizin5 <andeboizin5@gmail.com>
2023-09-13 08:21:57 -0700

    1-array_iterator.c

---
+++ 1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ * @array: array
+ * @size: size
+ * @action: action
+ * Return: Always 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
