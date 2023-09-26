cac7e5b66751eb896d55b9e192767afe4bdee648
Andeboizin5 <andeboizin5@gmail.com>
2023-09-26 06:22:13 -0700

    6-pop_listint.c

---
+++ 6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: input
+ * Return: Always 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	i = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (i);
+}
