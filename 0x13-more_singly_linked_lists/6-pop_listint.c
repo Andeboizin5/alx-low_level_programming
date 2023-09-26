c044eb22fba2b59010bf01f68392a16de357128a
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-09-26 15:39:09 +0200

    changes

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,8 +10,8 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int i;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
+	if (head != NULL || *head != NULL)
+		return (0);
 
 	i = (*head)->n;
 	temp = (*head)->next;
