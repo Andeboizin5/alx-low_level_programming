108c9a501d6395b124cb13f49e7226c6574e08cd
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-09-27 11:36:46 +0200

    changes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (num < index - 1)
 	{
-		if (tmp != NULL || !(tmp->next))
+		if (tmp == NULL || (tmp->next == NULL))
 			return (-1);
 		tmp = tmp->next;
 		num++;
