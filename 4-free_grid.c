e10195728c760c8c0d87f0c1b7f5e4ef8e277d6f
root <andeboizin5@gmail.com>
2023-10-30 08:58:15 -0700

    4-free_grid.c

---
+++ 4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: grid input
+ * @height: height input
+ * Return: Always 0
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
