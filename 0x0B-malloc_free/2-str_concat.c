85a33c3103898c24b87aaeb60389201a9f549b8e
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-10-31 06:32:24 +0200

    changes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int i, j = 0;
+	int i =0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -19,7 +19,6 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	i = 0;
 	while (s1[i] != '\0')
 		i++;
 
@@ -31,7 +30,6 @@ char *str_concat(char *s1, char *s2)
 	if (s == NULL)
 		return (NULL);
 
-	i = 0;
 	while (s1[i] != '\0')
 	{
 		s[i] = s1[i];
