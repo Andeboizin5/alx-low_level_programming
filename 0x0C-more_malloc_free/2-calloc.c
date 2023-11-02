935f7ccd371856290a6cc9687c167c892d2d570d
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-11-02 13:36:03 +0200

    changes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, j;
+	unsigned int i;
 	char *s;
 
 	if (nmemb == 0 || size == 0)
@@ -17,8 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (s == NULL)
 		return (NULL);
 
-	j = nmemb * size;
-	for (i = 0; i < j; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		s[i] = 0;
 	return (s);
 }
