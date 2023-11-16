4ff0b7c92b72896c6a80baf860036f96b36c5d26
Andeboizin5 <andeboizin5@gmail.com>
2023-11-15 23:26:30 -0800

    0-print_list.c

---
+++ 0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head input
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		i = h->next;
+		i++;
+	}
+	return (i);
+}
