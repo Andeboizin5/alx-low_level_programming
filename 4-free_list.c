5c9166041cea8d847bc42257b40db46d910ed3b0
Andeboizin5 <andeboizin5@gmail.com>
2023-11-16 04:21:52 -0800

    4-free_list.c

---
+++ 4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head input
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
