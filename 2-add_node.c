42a0b9cd353f0d51237acfa1c7f23a67ad1b8243
Andeboizin5 <andeboizin5@gmail.com>
2023-11-15 23:46:32 -0800

    2-add_node.c

---
+++ 2-add_node.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: head input
+ * @str: string input
+ * Return: Always 0
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int i = 0;
+	list_t *newnode;
+
+	while (str[i])
+		i++;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+	newnode->i = i;
+	newnode->next = (*head);
+	(*head) = newnode;
+
+	return (*head);
+}
