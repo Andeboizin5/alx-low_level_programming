aab7d4c3a0617b371dfa5a3faf2c1a40835e2d86
Andeboizin5 <andeboizin5@gmail.com>
2023-11-23 07:29:48 -0800

    5-flip_bits.c

---
+++ 5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * @n: first input
+ * @m: second input
+ * Return: Always 0
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i;
+	unsigned long int j;
+
+	i = n ^ m;
+
+	for (j = 0; j < i; j++)
+	{
+		if ((i & 1) == 1)
+			j++;
+		i = i >> 1;
+	}
+
+	return (j);
+}
