a3dcfa938df7d46f600c555970e0fc075aff2811
Andeboizin5 <andeboizin5@gmail.com>
2023-11-23 11:18:11 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[j] != '0' && b[j] != '1')
+		if (b[j] < '0' || b[j] > '1')
 			return (0);
 		j = 2 * j + (b[i] - '0')
 	}
