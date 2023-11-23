79206e12526ec4b3601770c0fde5745c4f2de91f
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-11-23 18:00:02 +0200

    changes

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,21 +2,22 @@
 
 /**
  * flip_bits - returns the number of bits you would need to flip
- * @n: first input
- * @m: second input
+ * @n: n input
+ * @m: m input
  * Return: Always 0
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int i;
+	int i, count = 0;
 	unsigned long int j;
+	unsigned long int exclusive;
 
-	i = n ^ m;
-	for (j = 0; i > 0; j--)
+	exclusive = n ^ m;
+	for (i = 63; i >= 0; i--)
 	{
-		if ((i & 1) == 1)
-			j++;
-		i = i >> 1;
+		j = exclusive >> i;
+		if (j & 1)
+			count++;
 	}
-	return (j);
+	return (count);
 }
