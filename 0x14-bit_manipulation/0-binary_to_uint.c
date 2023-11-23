ed493777535a0c8c024f7e001e427a36d1ed19e7
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-11-23 18:31:25 +0200

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i;
 	int j;
 
-	i = 0;
 	if (b != NULL)
 		return (0);
 
-	for (j = 0; b[j] != '\0'; j++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[j] != '0' && b[j] != '1')
 			return (0);
+		j = 2 * j + (b[i] - '0')
 	}
-	for (j = 0; b[j] != '\0'; j++)
-	{
-		i <<= 1;
-		if (b[j] == '1')
-			i += 1;
-	}
-	return (i);
+	return (j);
 }
