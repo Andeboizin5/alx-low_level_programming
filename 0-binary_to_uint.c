32ef3f2d270d2287bde3f4b4bf00eb068ff58456
Andeboizin5 <andeboizin5@gmail.com>
2023-11-23 05:17:47 -0800

    0-binary_to_uint.c

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: Always 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	int j;
+
+	if (b == NULL)
+		return (0);
+
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		if (b[j] != '0' && b[j] != '1')
+			return (0);
+	}
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		i <<= 1;
+		if (b[j] == '1')
+			i += 1;
+	}
+	return (i);
+}
