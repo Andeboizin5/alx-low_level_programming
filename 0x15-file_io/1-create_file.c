636a11fa2578367ff81e26f566354d328de58fa2
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-12-01 19:29:44 +0200

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i]; i++)
-		i++;
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600)
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; text_content[i];)
+		i++;
+
 	w = write(fd, text_content, i);
 
-	if (fd == -1 || w == -1)
+	if (w == -1)
 		return (-1);
 
 	close(fd);
