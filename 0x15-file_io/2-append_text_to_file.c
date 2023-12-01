02be450ab122d9ada6cba1495fe4049420861d71
Andeboizin5 <64868276+Andeboizin5@users.noreply.github.com>
2023-12-01 18:07:46 +0200

    changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,8 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (i = 0; text_content[i]; i++)
-			;
+		for (i = 0; text_content[i];)
+			i++;
 
 		w = write(fd, text_content, i);
 
